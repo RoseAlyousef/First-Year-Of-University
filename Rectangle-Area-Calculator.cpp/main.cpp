5879e047dffff6ab4fe3621363f8913b779e0b2f
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-04-22 17:39:31 +0300

    Educational Code

---
+++ Rectangle-Area-Calculator.cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    double width, length, area;
+
+    cout << "enter the width" << endl;
+    cin >> width;
+
+    cout << "enter the length" << endl;
+    cin >> length;
+
+    area = width * length;
+
+    cout << "area is " << "  " << area << endl;
+
+    return 0;
+}
