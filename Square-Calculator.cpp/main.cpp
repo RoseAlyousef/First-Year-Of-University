93b37958a1f1ab93682da1d282e7a910fe547abe
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-04-24 21:57:23 +0300

    Educational Code

---
+++ Square-Calculator.cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int square(int num) {
+    return num * num;
+}
+
+int main() {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+
+    cout << "Square of " << x << " is: " << square(x) << endl;
+
+    return 0;
+}
