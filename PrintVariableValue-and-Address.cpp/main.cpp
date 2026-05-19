86ff00415b1f139f97ceb593f14c699484a8da4c
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-05-20 00:07:13 +0300

    Educational Code

---
+++ PrintVariableValue-and-Address.cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 23;
+    cout << "the value of n is: n= " << n << endl;
+    cout << "the address of n is: &n= " << &n << endl;
+
+    return 0;
+}
