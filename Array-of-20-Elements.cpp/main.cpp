720a04e2e514924b1897252d328aa60c97d478a8
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-05-19 23:32:20 +0300

    Educational Code

---
+++ Array-of-20-Elements.cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A[20];
+    for (int i = 0; i < 20; i++) {
+        cin >> A[i];
+    }
+    for (int i = 0; i < 20; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
