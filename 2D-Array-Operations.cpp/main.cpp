3dac06520642d932a4d87b9ec0b367d654107ea2
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-06-11 22:12:26 +0300

    Educational Code

--- 2D-Array-Operations.cpp/main.cpp
+++ 2D-Array-Operations.cpp/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int arr[5][3];
+    int sum = 0;
+
+    cout << "Enter elements for a 5x3 array:\n";
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+            sum += arr[i][j];
+        }
+    }
+    cout << "\nThe array is:\n";
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "\nSum of all elements = " << sum << endl;
+    cout << "\nOutput based on condition (row > column):\n";
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i > j) {
+                cout << arr[i][j] << "\t";
+            } else {
+                cout << 0 << "\t";
+            }
+        }
+        cout << endl;
+    }
+
     return 0;
 }
