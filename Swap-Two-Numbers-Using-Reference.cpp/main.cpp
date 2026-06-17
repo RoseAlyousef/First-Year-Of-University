bf541709928e80861b98937c40868c0cd8aab099
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-06-17 23:58:05 +0300

    educationa; code

--- Swap-Two-Numbers-Using-Reference.cpp/main.cpp
+++ Swap-Two-Numbers-Using-Reference.cpp/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
 using namespace std;
+void swapNumbers(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int x = 5, y = 10;
+
+    cout << "Before swap: x = " << x << ", y = " << y << endl;
+
+    swapNumbers(x, y);
+
+    cout << "After swap: x = " << x << ", y = " << y << endl;
+
     return 0;
 }
