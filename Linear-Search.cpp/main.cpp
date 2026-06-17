b23e3c3646fc39d72df39fb6aa102d0de3be2e97
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-06-17 23:49:32 +0300

    Educactional Code

--- Linear-Search.cpp/main.cpp
+++ Linear-Search.cpp/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
 using namespace std;
+int main() {
+   const int SIZE = 5;
+   int arr[SIZE];
+   int searchKey;
+   bool found = false;
+   cout << "Enter 5 elements:\n";
+   for (int i = 0; i < SIZE; i++) {
+       cin >> arr[i];
+   }
+   cout << "Enter the element to search for: ";
+   cin >> searchKey;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+   for (int i = 0; i < SIZE; i++) {
+       if (arr[i] == searchKey) {
+           cout << "Element found at index: " << i << endl;
+           found = true;
+           break;
+       }
+   }
+   if (!found) {
+       cout << "Element not found in the array." << endl;
+   }
+   return 0;
 }
