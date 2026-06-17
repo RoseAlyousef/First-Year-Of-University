ade8a31641515df643f10597e639dafd502d54a6
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-06-17 23:53:37 +0300

    educational code

--- Calculate-Average-and-Maximum-of-Three-Numbers.cpp/main.cpp
+++ Calculate-Average-and-Maximum-of-Three-Numbers.cpp/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
 using namespace std;
+void changeByValue(int x) {
+    x = 100;
+}
+void changeByReference(int &x) {
+    x = 100;
+}
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int a = 10;
+    changeByValue(a);
+    cout << "Value after changeByValue: " << a << " (Stays 10)" << endl; // تبقى القيمة 10
+    changeByReference(a);
+    cout << "Value after changeByReference: " << a << " (Becomes 100)" << endl; // تصبح القيمة 100
+
     return 0;
 }
