92b4b04a2d54f54755a841befc9bc7b5535157be
RoseAlyousef <rosesalehalyousef2006@gmail.com>
2026-06-18 00:01:50 +0300

    educational code

--- Count-Even-and-Odd-Numbers-via-Reference.cpp/main.cpp
+++ Count-Even-and-Odd-Numbers-via-Reference.cpp/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
 using namespace std;
+void countEvenOdd(int a, int b, int c, int &evenCount, int &oddCount) {
+    evenCount = oddCount = 0;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    if (a % 2 == 0) evenCount++; else oddCount++;
+    if (b % 2 == 0) evenCount++; else oddCount++;
+    if (c % 2 == 0) evenCount++; else oddCount++;
+}
+
+int main() {
+    int x, y, z;
+    cout << "Enter three numbers: ";
+    cin >> x >> y >> z;
+
+    int evenCount, oddCount;
+
+    countEvenOdd(x, y, z, evenCount, oddCount);
+
+    cout << "Even numbers count = " << evenCount << endl;
+    cout << "Odd numbers count = " << oddCount << endl;
+
     return 0;
 }
